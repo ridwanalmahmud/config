f86a0d621c683910f7be7045d309d054eef7ee17
ridwanalmahmud <ridwanalmahmud107@gmail.com>
2025-05-19 13:59:35 +0000

    adding luasnip custom snippet and changing to local time in tmux

---
+++ nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,53 @@
+return {
+    "L3MON4D3/LuaSnip",
+    config = function()
+        local ls = require "luasnip"
+        local s = ls.snippet
+        local t = ls.text_node
+        local i = ls.insert_node
+
+        vim.keymap.set({ "i", "s" }, "<A-j>", function ()
+            if ls.expand_or_jumpable() then
+                ls.expand_or_jump()
+            end
+        end, { silent = true })
+
+        vim.keymap.set({ "i", "s" }, "<A-k>", function ()
+            if ls.jumpable(-1) then
+                ls.jump(-1)
+            end
+        end, { silent = true })
+
+        ls.add_snippets("make", {
+            s(
+              "makefile_basic",
+              {
+                t({ "CC=gcc" }),
+                t({ "", "ICDIRS=-I" }),
+                t({ "", "OPT=-O1" }),
+                t({ "", "CFLAGS=-Wall -Wextra -g $(INCDIRS) $(OPT)" }),
+                t({ "", "" }),
+                t({ "", "CFILES=" }),i(1, "src/main.c"),
+                t({ "", "BINARY=" }),i(2, "bin/main"),
+                t({ "", "" }),
+                t({ "", "all: $(BINARY)" }),
+                t({ "", "" }),
+                t({ "", "$(BINARY):" }),
+                t({ "", "\t$(CC) $(CFLAGS) $(CFILES) -o $@" }),
+                t({ "", "" }),
+                t({ "", "run:" }),
+                t({ "", "\t$(BINARY)" }),
+                t({ "", "" }),
+                t({ "", "clean:" }),
+                t({ "", "\trm -f $(BINARY)" }),
+              },
+              {
+                priority = 100,
+                description = "Basic Makefile with variables",
+                keywords = {"makefile", "c", "compile"},
+              }
+            )
+        })
+
+    end
+}
